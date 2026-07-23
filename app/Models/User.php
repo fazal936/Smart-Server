18106c0062be225df7a23efc9c33a4d414262dbb
Fazal936 <fsubhan655@gmail.com>
2026-07-23 11:07:20 +0000

    feat: add staff roles and active status

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
  */
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role' ,'is_active'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable
 {
